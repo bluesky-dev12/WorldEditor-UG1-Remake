efff897a71c9f42c4a2716f6d1996bd74d24fc8a
bluesky-dev12 <ravello17@gmail.com>
2023-07-15 04:37:53 +0100

    Objects initial add stuff

---
+++ Core/Chunks/Solids/UG1Objects.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Chunks.Solids
+{
+    internal class UG1Objects
+    {
+    }
+}
