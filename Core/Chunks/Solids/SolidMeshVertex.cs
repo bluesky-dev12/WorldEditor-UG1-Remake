e8ab2c3f12c5952337e46dac9b19658d2793eaae
bluesky-dev12 <ravello17@gmail.com>
2023-07-16 05:16:09 +0100

    Corrected branch files.

---
+++ Core/Chunks/Solids/SolidMeshVertex.cs
@@ -0,0 +1,19 @@
+﻿using System.Numerics;
+
+namespace Core.Solids
+{
+    public struct SolidMeshVertex
+    {
+        // REQUIRED attributes
+        public Vector3 Position { get; set; }
+        public Vector2 TexCoords { get; set; }
+
+        // OPTIONAL attributes
+        public Vector3? Normal { get; set; }
+        public Vector3? Tangent { get; set; }
+        public Vector3? BlendWeight { get; set; }
+        public Vector3? BlendIndices { get; set; }
+
+        public uint? Color { get; set; }
+    }
+}
